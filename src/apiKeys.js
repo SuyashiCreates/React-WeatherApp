baa065c7719279b84821bdd7a7e9f6d660e1f406
SuyashiCreates <suyashisainath1@gmail.com>
2024-06-08 17:02:35 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "{11dd7576452e7203e2a7dbdf6a61aeab}",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
